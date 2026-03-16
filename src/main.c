a976950144d4eec2b7c623bf44fcf20d515df21d
0ropo <he202225@students.ephec.be>
2026-03-16 23:36:46 +0100

    (feat): testing archive.name and archive.size specifically

--- src/main.c
+++ src/main.c
@@ -3,10 +3,25 @@
 #include <string.h>
 #include "help.h"
 
+/*
+unsigned int calculate_checksum(struct tar_t* entry){
+    memset(entry->chksum, ' ',8);
+    unsigned int check = 0;
+    unsigned char* raw = (unsigned char*) entry;
+    for(int i = 0; i < 512; i++){
+        check += raw[i];
+    }
+    snprintf(entry->chksum, sizeof(entry->chksum), "%06o0", check);
+        entry->chksum[6] = '\0';
+        entry->chksum[7] = ' ';
+        return check;
+        }
+*/
+
 // Fonctions of differents types of fuzzing
 
 void init_clean_archive(struct tar_t* archive){
-    memset(archive, 0, sizeof(struct tar_t)); // cleanup of mémoire
+    memset(archive,0,sizeof(struct tar_t)); // cleanup of mémoire
     strcpy(archive->name, "test.txt");
     strcpy(archive->mode, "0000777");
     strcpy(archive->uid, "0000000");
@@ -21,15 +36,16 @@ void init_clean_archive(struct tar_t* archive){
 void save_archive(struct tar_t* archive, const char* filename){
     calculate_checksum(archive);
 
-    FILE *file = fopen(filename, "wb");
+    FILE *file = fopen(filename,"wb");
 
     if (file != NULL){
-        fwrite(archive, sizeof(struct tar_t), 1, file);
+        fwrite(archive, sizeof(struct tar_t),1,file);
         fclose(file);
         printf("Created archive successfuly : %s\n", filename);
     }
+
     else{
-        printf("Error creating the file : %s\n", filename);
+        printf("Error creating the file : %s\n",filename);
     }
 }
 
@@ -40,13 +56,12 @@ void test_attack(int argc, char* argv[], struct tar_t* archive, const char* name
 
     if (result == 1){
         printf("[OK]Success ! Crash detected !\n");
-        save_archive(archive, name_succes);
-        printf("Archive saved under the name : %s\n", name_succes);
+        save_archive(archive,name_succes);
+        printf("Archive saved under the name : %s\n",name_succes);
+        printf("[KO]Failed !");
     }
-	else{
-		printf("[KO]Failed !");
-	}
 }
+
 
 int main(int argc, char* argv[]){
     if(argc < 2){
@@ -54,21 +69,53 @@ int main(int argc, char* argv[]){
         return -1;
     }
 
+
     struct tar_t archive;
 
     printf("--- Starting generation of archive---\n\n");
 
     // Starting with a clean archive
     init_clean_archive(&archive);
-    save_archive(&archive, "archive.tar");
+    save_archive(&archive,"archive.tar");
+    printf("--- Starting Fuzzing on archive.name: ---\n\n");
 
     // First test buffer overflow
-    printf("--- [1] Buffer Overflow testing with 100 ---\n\n");
+    printf("--- [1] Fuzzing on name Buffer Overflow testing with 100 ---\n\n");
 
     init_clean_archive(&archive);
-    memset(archive.name, 'A', 100);
-    save_archive(&archive, "archive.tar");
-    test_attack(argc, argv, &archive, "success_buffer_overflow.tar");
+    memset(archive.name,'A', 100);
+    save_archive(&archive,"archive.tar");
+    test_attack(argc,argv,&archive,"success_buffer_overflow.tar");
+
+
+    printf("--- [2] Fuzzing on archive.name with formating ---\n\n");
+    init_clean_archive(&archive);
+    strcpy(archive.name,"%s%s%s%s%s");
+    save_archive(&archive,"archive.tar");
+    test_attack(argc,argv,&archive,"success_archivename.tar");
+
+
+    printf("--- Starting Fuzzing on archive.size: ---\n\n");
+    printf("--- [1] Fuzzing on archive.size with null size ---\n\n");
+    init_clean_archive(&archive);
+    strcpy(archive.size,"0");
+    save_archive(&archive,"archive.tar");
+    test_attack(argc,argv,&archive,"success_nullsize.tar");
+
+    printf("--- Starting Fuzzing on archive.size: ---\n\n");
+    printf("--- [2] Fuzzing on archive.size with null size ---\n\n");
+    init_clean_archive(&archive);
+    strcpy(archive.size,"-1");
+    save_archive(&archive,"archive.tar");
+    test_attack(argc,argv,&archive,"success_negativesize.tar");
+
+    printf("--- [3] Fuzzing on archive.size with negative size ---\n\n");
+    init_clean_archive(&archive);
+    memset(archive.size,'9',12);
+    save_archive(&archive,"archive.tar");
+    test_attack(argc,argv,&archive,"success_ultrabigsize.tar");
+
+
 
     return 0;
 }
